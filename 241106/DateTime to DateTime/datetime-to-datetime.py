4d72c14a0f03bbb7a6e32f59eef2b1e2fb68bda3
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2024-11-06 10:46:31 +0900

    Title: DateTime to DateTime, Time: 41ms, Memory: 15MB, Status: Passed - CodeTree

--- 241106/DateTime to DateTime/datetime-to-datetime.py
+++ 241106/DateTime to DateTime/datetime-to-datetime.py
@@ -1,4 +1,5 @@
 a,b,c = map(int, input().split())
 before = 11*24*60 + 11*60 + 11
 after = a*24*60 + b*60 + c
-print(after - before)
+date_time = after - before
+print(date_time if date_time>=0 else -1)
