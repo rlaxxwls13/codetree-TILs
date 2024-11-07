b24d54682d5254200b94e8cbe3605e5b368c2f78
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2024-11-07 18:27:21 +0900

    Title: 아름다운 수, Time: 44ms, Memory: 15MB, Status: Passed - CodeTree

--- 241107/아름다운 수/beautiful-number.py
+++ 241107/아름다운 수/beautiful-number.py
@@ -3,13 +3,14 @@ count = 0
 
 def beautiful_number(curr_num):
     global count
-    if curr_num == n + 1:
-        count += 1
-        return
-    if curr_num > n:
+    if curr_num >= n + 1:
+        if curr_num == n + 1:
+            count += 1
         return
         
     for i in range(1, 5):
+        if curr_num + i > n + 1:
+            break
         beautiful_number(curr_num + i)
     return
 
