25489bf2d9e026a5947271dbcecdd64669356207
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2024-11-10 14:12:38 +0900

    Title: xor 결과 최대 만들기, Time: 54ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241110/xor 결과 최대 만들기/max-of-xor.py
@@ -0,0 +1,28 @@
+from functools import reduce
+
+n, m = map(int, input().split())
+num_list = list(map(int, input().split()))
+
+ans = 0
+
+selected = []
+
+def xor():
+    return reduce(lambda x, y: x^y, selected) 
+
+def choose(cnt):
+    global ans
+    if cnt == m:
+        ans = max(ans, xor())
+        return
+
+    for i in range(1, n+1):
+        if i in selected:
+            continue
+        selected.append(i)
+        choose(cnt + 1)
+        selected.pop()
+
+
+choose(0)
+print(ans)
