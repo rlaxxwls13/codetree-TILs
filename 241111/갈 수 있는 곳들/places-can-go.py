d09a7f6f9ee6830b21e17bd4e09a785060038818
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2024-11-11 16:01:57 +0900

    Title: 갈 수 있는 곳들, Time: 5100ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

---
+++ 241111/갈 수 있는 곳들/places-can-go.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+n, k = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+start_x, start_y = [], []
+for _ in range(k):
+    x, y = map(int, input().split())
+    start_x.append(x-1)
+    start_y.append(y-1)
+
+visited = [[False for _ in range(n)] for _ in range(n)]
+
+ans = 0
+
+q = deque()
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def can_go(x, y, curr_visited):
+    return in_range(x, y) and grid[x][y] == 0 and curr_visited[x][y] == False
+
+def bfs(curr_visited):
+    dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
+    
+    while q:
+        x, y = q.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if can_go(nx, ny, curr_visited):
+                curr_visited[nx][ny] = True
+                visited[nx][ny] =True
+                q.append((nx, ny))
+
+for x, y in zip(start_x, start_y):
+    curr_visited = [[False for _ in range(n)] for _ in range(n)]
+    curr_visited[x][y] = True
+    visited[x][y] = True
+    q.append((x, y))
+    bfs(curr_visited)
+
+for i in range(n):
+    for j in range(n):
+        if visited[i][j] == True:
+            ans += 1
+print(ans)
