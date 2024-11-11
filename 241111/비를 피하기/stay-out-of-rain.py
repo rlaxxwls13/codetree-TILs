d57b80732da100570ceb21e1a7533bd4572bc1ec
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2024-11-11 20:39:50 +0900

    Title: 비를 피하기, Time: 115ms, Memory: 21MB, Status: Passed - CodeTree

--- 241111/비를 피하기/stay-out-of-rain.py
+++ 241111/비를 피하기/stay-out-of-rain.py
@@ -1,3 +1,4 @@
+"""
 from collections import deque
 
 n, h, m = map(int, input().split())
@@ -80,4 +81,70 @@ for x, y in people:
 for i in range(n):
     for j in range(n):
         print(ans_grid[i][j], end=" ")
+    print()
+
+"""
+
+### bfs 한번으로만 구할 수 있음 ###
+# 사람이 기준이 아니라 쉘터를 기준으로 하면,,
+# 각 쉘터를 q에 추가함, 해당 q를 가지고 bfs를 돌림
+# 돌리다가 visited=True를 만나면 최단거리가 짧은 다른 쉘터가 이미 지나감,
+# 그러므로 이미 가장 가까운 쉘터가 아님
+
+# 뭔가,, 서로 다른 사람들이 같은 장소를 향한 최단거리를 구할때
+# 사람마다 구하는거보단 최단거리에서 사람들을 향한 최단거리를 한번에 구하는게 훨 낫겠지
+# 그리고 쉘터가 여러개여도 최단거리가 아닌것들은 탈락되기때문에 visited를 공유해도
+# 상관이 없음 step을 같게 시작하기 때문에
+# 가려는 위치의 visited = True이면 이미 이전 스텝에서 그 자리를 차지한 쉘터가 있다는 뜻
+
+from collections import deque
+
+n, h, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False for _ in range(n)] for _ in range(n)]
+step = [[-1 for _ in range(n)] for _ in range(n)]
+
+q = deque()
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def can_go(x, y):
+    return in_range(x, y) and visited[x][y] == False and grid[x][y] != 1
+
+def push(x, y, s):
+    q.append((x, y))
+    visited[x][y] = True
+    step[x][y] = s
+
+def bfs():
+    dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
+
+    while q:
+        x, y = q.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if can_go(nx, ny):
+                s = step[x][y] + 1
+                push(nx, ny, s)
+
+
+s_pos = []
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 3:
+            s_pos.append((i, j))
+
+for x, y in s_pos:
+    push(x, y, 0)
+    
+bfs()
+
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 2:
+            print(step[i][j], end=" ")
+        else:
+            print(0, end=" ")
     print()
