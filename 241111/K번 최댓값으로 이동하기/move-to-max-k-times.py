ec055813234ee11e2c499f862534abc75229e9d7
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2024-11-11 18:33:07 +0900

    Title: K번 최댓값으로 이동하기, Time: 58ms, Memory: 16MB, Status: Passed - CodeTree

--- 241111/K번 최댓값으로 이동하기/move-to-max-k-times.py
+++ 241111/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -6,14 +6,16 @@ curr_x, curr_y = map(lambda a: int(a) - 1, input().split())
 
 dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
 
+
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
+
 def can_go(x, y):
-    return in_range(x, y) and visited[x][y] == False 
+    return in_range(x, y) and visited[x][y] == False
+
 
 def bfs():
-
     while q:
         x, y = q.popleft()
 
@@ -24,13 +26,14 @@ def bfs():
                 adjacent.append((nx, ny))
                 visited[nx][ny] = True
 
+
 for _ in range(k):
 
     # bfs로 갈 수 있는 칸 구하기
     q = deque()
     visited = [[False for _ in range(n)] for _ in range(n)]
     adjacent = []
-    
+
     q.append((curr_x, curr_y))
     visited[curr_x][curr_y] = True
     bfs()
@@ -47,12 +50,11 @@ for _ in range(k):
     nx, ny = -1, -1
     for i in range(n):
         for j in range(n):
-            if grid[i][j] == mx:
+            if (i,j) in adjacent and grid[i][j] == mx:
                 nx, ny = i, j
                 break
         if nx != -1:
             break
     curr_x, curr_y = nx, ny
-
 
 print(curr_x + 1, curr_y + 1)
