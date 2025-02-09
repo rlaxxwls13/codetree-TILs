228f9237edc37f2da18783fbfe1d74eb679412f4
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2025-02-10 00:21:49 +0900

    Title: 숫자 합치기, Time: 219ms, Memory: 23MB, Status: Passed - CodeTree

--- 250210/숫자 합치기/merge-numbers.py
+++ 250210/숫자 합치기/merge-numbers.py
@@ -1,8 +1,11 @@
+import heapq
+
 n = int(input())
 arr = list(map(int, input().split()))
 
 # Write your code here!
 
+"""
 ans = 0
 
 while(len(arr) > 1):
@@ -12,6 +15,22 @@ while(len(arr) > 1):
     arr.remove(min2)
     arr.append(min1+min2)
     ans += min1+min2
+
+print(ans)
+"""
+
+pq = []
+ans = 0
+
+for elem in arr:
+    heapq.heappush(pq, elem)
+
+while len(pq) > 1:
+    x1 = heapq.heappop(pq)
+    x2 = heapq.heappop(pq)
+
+    ans += (x1 + x2)
+    heapq.heappush(pq, x1 + x2)
 
 print(ans)
 
