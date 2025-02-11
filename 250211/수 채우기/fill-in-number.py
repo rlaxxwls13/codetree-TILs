7995c826f4f1ca995cf304a7c8c37ee78ccbf7eb
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2025-02-11 23:32:07 +0900

    Title: 수 채우기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250211/수 채우기/fill-in-number.py
+++ 250211/수 채우기/fill-in-number.py
@@ -3,10 +3,12 @@ n = int(input())
 # Write your code here!
 
 ans = 0
-
-if (n % 5) % 2 == 0:
-    ans = (n // 5) + (n % 5) // 2
+if n == 1 or n == 3:
+    ans = -1
 else:
-    ans = (n // 5 - 1) + (n % 5 + 5) // 2
+    if (n % 5) % 2 == 0:
+        ans = (n // 5) + (n % 5) // 2
+    else:
+        ans = (n // 5 - 1) + (n % 5 + 5) // 2
 
 print(ans)
