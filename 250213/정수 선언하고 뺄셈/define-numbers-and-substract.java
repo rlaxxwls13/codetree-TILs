25a04a08e87f2ed8269ac5d98d88d1612d6d73cc
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2025-02-13 03:47:58 +0900

    Title: 정수 선언하고 뺄셈, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250213/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 97, b = 13;
+        int c = a - b;
+        System.out.print(a + " - " + b + " = " + c);
+    }
+}
