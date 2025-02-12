21217f8fbb4bbd476b1ef24f8ca77ac220e69bee
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2025-02-13 02:41:03 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 54ms, Memory: 19MB, Status: Passed - CodeTree

--- 250213/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 250213/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -4,32 +4,13 @@ price = list(map(int, input().split()))
 # Write your code here!
 
 ans = 0
+curr_min = float('inf')
 
 # 차가 최소여야함
-# 사는날이 최소이거나 파는날이 최대 둘중 하나는 반드시..?
 
-# buy-min
-
-buy_min = float('inf')
-buy_min_idx = 0
-
-for i in range(n):
-    if price[i] < buy_min:
-        buy_min = price[i]
-        buy_min_idx = i
-
-for i in range(buy_min_idx, n):
-    ans = max(ans, price[i] - buy_min)
-
-sell_max = -1
-sell_max_idx = 0
-
-for i in range(n):
-    if price[i] > sell_max:
-        sell_max = price[i]
-        sell_max_idx = i
-
-for i in range(sell_max_idx + 1):
-    ans = max(ans, sell_max - price[i])
+# i는 파는 시점
+for i in range(1, n):
+    curr_min = min(curr_min, price[i-1])
+    ans = max(ans, price[i] - curr_min)
 
 print(ans)
