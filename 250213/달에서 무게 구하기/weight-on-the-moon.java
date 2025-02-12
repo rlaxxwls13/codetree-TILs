80f6179da648d3199e473a2adba3ca15ba31b1c8
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2025-02-13 04:12:16 +0900

    Title: 달에서 무게 구하기, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250213/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int weigh = 13;
+        double ratio = 0.165;
+        System.out.printf("%d * %.6f = %.6f", weigh, ratio, weigh * ratio);
+    }
+}
