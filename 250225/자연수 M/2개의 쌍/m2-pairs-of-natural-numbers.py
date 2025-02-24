021baaf2d527cc5e380a0301690d72a1df371fb5
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2025-02-25 05:33:08 +0900

    Title: 자연수 M/2개의 쌍, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250225/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 250225/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -12,13 +12,16 @@ sorted_x, sorted_y = zip(*sorted(list(zip(x, y)), key=lambda x: x[1]))
 sorted_x, sorted_y = list(sorted_x), list(sorted_y)
 
 start, end = 0, N - 1
+i = 0
 
-for i in range(M//2):
+while(i <= M//2):
     if i > sum(sorted_x[:start + 1]):
         start += 1
     if i > sum(sorted_x[end:]):
         end -= 1
+
     ans = max(ans, sorted_y[start] + sorted_y[end])
+    i += min(sorted_x[start], sorted_x[end])
 
 
 print(ans)
