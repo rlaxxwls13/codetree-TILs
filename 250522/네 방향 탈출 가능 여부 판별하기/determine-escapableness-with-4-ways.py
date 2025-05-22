65486d03dcc824069a9c3fcf1e77dfe10f78baaa
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2025-05-22 20:17:29 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 83ms, Memory: 19MB, Status: Passed - Codetree

--- 250522/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 250522/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -8,21 +8,22 @@ def can_go(x, y):
     return 0 <= x < n and 0 <= y < m and visited[x][y] == 0 and a[x][y] == 1
 
 def bfs():
-    x, y = q.popleft()
+    while q:
+        x, y = q.popleft()
 
-    dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
-    for dx, dy in zip(dxs, dys):
-        nx, ny = x + dx, y + dy
-        if can_go(nx, ny):
-            q.append((nx, ny))
-            visited[nx][ny] = 1
-            bfs()
+        dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if can_go(nx, ny):
+                q.append((nx, ny))
+                visited[nx][ny] = 1
 
 
 q = deque()
 visited = [[0 for _ in range(m)] for _ in range(n)]
 
 q.append((0, 0))
+visited[0][0] = 1
 bfs()
 
 print(visited[n-1][m-1])
