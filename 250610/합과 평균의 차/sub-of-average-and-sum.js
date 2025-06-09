609a0ec82df1ad5ddc8fca7768f3d87ffe1cdb4a
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2025-06-10 05:07:29 +0900

    Title: 합과 평균의 차, Time: 171ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250610/합과 평균의 차/sub-of-average-and-sum.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+let sum = a + b + c;
+let mean = sum / 3;
+console.log(sum);
+console.log(mean);
+console.log(sum - mean);
