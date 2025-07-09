7721061955b1f8174d303a1dcc75025157985980
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2025-07-09 15:23:22 +0900

    Title: 학점 계산기, Time: 150ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 250709/학점 계산기/credit-calculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double[] arr = new double[n];
+        double sum = 0;
+        double avg;
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextDouble();
+            sum += arr[i];
+        }
+        avg = sum/n;
+        System.out.printf("%.1f\n", avg);
+        if (avg >= 4.0) {
+            System.out.print("Perfect");
+        } else if (avg >= 3.0) {
+            System.out.print("Good");
+        } else {
+            System.out.print("Poor");
+        }
+    }
+}
