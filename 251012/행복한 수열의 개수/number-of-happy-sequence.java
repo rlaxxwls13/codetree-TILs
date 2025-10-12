5ed4df83e298e3517177c94b8eaaccdd9624ab33
rlaxxwls13 <101396454+rlaxxwls13@users.noreply.github.com>
2025-10-12 17:58:51 +0900

    Title: 행복한 수열의 개수, Time: 218ms, Memory: 15MB, Status: Passed - Codetree

--- 251012/행복한 수열의 개수/number-of-happy-sequence.java
+++ 251012/행복한 수열의 개수/number-of-happy-sequence.java
@@ -5,6 +5,22 @@ public class Main {
     public static final int MAX_N = 100;
     public static int n, m;
     public static int[][] grid = new int[MAX_N][MAX_N];
+    public static int[] seq = new int[MAX_N];
+
+    public static int isHappySequence() {
+        int streak = 1, maxStreak = 1;
+        for (int i = 1; i < n; i++) {
+            if (seq[i] == seq[i-1])
+                streak += 1;
+            else
+                streak = 1;
+            maxStreak = Math.max(streak, maxStreak);
+        }
+        if (maxStreak >= m)
+            return 1;
+        else
+            return 0;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -16,16 +32,30 @@ public class Main {
         }
 
         int ans = 0;
-        int prev, streak;
 
         for (int i = 0; i < n; i++) {
-            prev = 0; streak = 1;
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == prev)
+                seq[j] = grid[i][j];
+            }
+            ans += isHappySequence();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                seq[j] = grid[j][i];
+            }
+            ans += isHappySequence();
+        }
+        /*
+        int streak;
+
+        for (int i = 0; i < n; i++) {
+            streak = 1;
+            for (int j = 1; j < n; j++) {
+                if (grid[i][j] == grid[i][j-1])
                     streak += 1;
                 else {
                     streak = 1;
-                    prev = grid[i][j];
                 }
                 //System.out.println(i + " " + j + " " + streak);
                 if (streak == m) {
@@ -36,13 +66,12 @@ public class Main {
         }
 
         for (int i = 0; i < n; i++) {
-            prev = 0; streak = 1;
-            for (int j = 0; j < n; j++) {
-                if (grid[j][i] == prev)
+            streak = 1;
+            for (int j = 1; j < n; j++) {
+                if (grid[j][i] == grid[j-1][i])
                     streak += 1;
                 else {
                     streak = 1;
-                    prev = grid[j][i];
                 }
 
                //System.out.println(i + " " + j + " " + streak);
@@ -52,6 +81,8 @@ public class Main {
                 }
             }
         }
+        */
+
         System.out.print(ans);
     }
 }
